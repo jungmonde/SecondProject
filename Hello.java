2c2446f93c937def448548147d4c7a6ef6396f63
jungmonde <jung77null@gmail.com>
2017-06-28 17:42:14 +0900

    add output Hello.java

--- Hello.java
+++ Hello.java
@@ -2,8 +2,12 @@ public class Hello{
 	public static void main(String[] args){
 		System.out.println("hello git");
 		System.out.println("what is git?");
+		System.out.println("That's DVCS");
+		System.out.println("Linux kernel eun 2002nyeon Bitkeeper ranun sangyong DVCS sayong ");
 	}
 
 
 }
+
+
 
